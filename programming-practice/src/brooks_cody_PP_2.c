454719d77886653fad0b007f529afeeb0727d714
Cody <codymb36@gmail.com>
2015-09-29 21:34:20 -0500

    Added program analysis questions, minor tweaks

--- programming-practice/src/brooks_cody_PP_2.c
+++ programming-practice/src/brooks_cody_PP_2.c
@@ -1,3 +1,30 @@
+/*
+Cody Brooks
+cmbrooks@iastate.edu
+CPRE 185 Section B
+Programming Practice 2
+<Reflection 1 What are you trying to learn or achieve?>
+    This program was intended to practice using functions and using conditional
+    statements. To do this, I designed a program where a user can guess a number
+    between 1 and 10. The response of the program was different based on how
+    the user's input compared to the secret number.
+<Reflection 2 Were you successful? Why or why not?>
+    Yes. The program correctly responds to the user based on the inputted guess.
+    I successfully implemented a function I defined and several conditional
+    statements so that I could get a range of response.
+<Reflection 3 Would you do anything differently if you started the program over? If so, explain what.>
+    I would try harder to get a new random number every time the prigram runs.
+    Along with this, adding the functionality to change the range of the secret
+    number. For example, instead of guessing a number between 1 and 10, perhaps
+    guessing a number between 12 and 68.
+<Reflection 4 Think about the most important thing you learned when writing this piece of code. What was it and explain why it was significant.>
+    Generating random numbers is a complex process compared to other languages
+    like Java and Python. I was also reminded that the order of if/else if
+    statements matters. I tried to check and see if the guess was within the
+    correct range at the end, and when I tested it, the "Higher" or "Lower"
+    conditionals triggered first.
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -19,7 +46,7 @@ int main() {
 
     while (!guessed) {
         guess = getGuess();
-        if (guess > 10) {
+        if (guess > 10 && guess < 1) {
             printf("What kind of guess was that? I said between 1 and 10!\n");
         } else if (guess < secretNumber) {
             printf("Higher\n");
